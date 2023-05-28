8ce8f2dc7886aaecdbd39418260732542b220bad
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2023-05-28 12:01:10 +0200

    Create an OR expression

---
+++ src/Expression/Boolean/Orx.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Arkitect\Expression\Boolean;
+
+use Arkitect\Analyzer\ClassDescription;
+use Arkitect\Expression\Description;
+use Arkitect\Expression\Expression;
+use Arkitect\Rules\Violation;
+use Arkitect\Rules\ViolationMessage;
+use Arkitect\Rules\Violations;
+
+final class Orx implements Expression
+{
+    /** @var Expression[] */
+    private $expressions;
+
+    public function __construct(Expression ...$expressions)
+    {
+        $this->expressions = $expressions;
+    }
+
+    public function describe(ClassDescription $theClass, string $because): Description
+    {
+        $expressionsDescriptions = [];
+        foreach ($this->expressions as $expression) {
+            $expressionsDescriptions[] = $expression->describe($theClass, $because)->toString();
+        }
+        $expressionsDescriptionsString = '['.implode('] OR [', array_unique($expressionsDescriptions)).']';
+        return new Description($expressionsDescriptionsString, $because);
+    }
+
+    public function evaluate(ClassDescription $theClass, Violations $violations, string $because): void
+    {
+        if (count($this->expressions) === 0) {
+            return;
+        }
+
+        foreach ($this->expressions as $expression) {
+            $newViolations = new Violations();
+            $expression->evaluate($theClass, $newViolations, '');
+            if (0 === $newViolations->count()) {
+                return;
+            }
+        }
+
+        $violations->add(
+            Violation::create(
+                $theClass->getFQCN(),
+                ViolationMessage::withDescription($this->describe($theClass, $because), "All OR expressions failed: ")
+            )
+        );
+    }
+}
