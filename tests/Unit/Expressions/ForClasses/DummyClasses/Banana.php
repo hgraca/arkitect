48ea64cfbc48677a6008f7dd41270554e9f17176
Herberto Graca <herberto.graca@lendable.co.uk>
2023-07-26 22:28:49 +0200

    Create a IsA expression
    This will allow for testing if a class extends or
    implements another code unit, anywhere in the inheritance tree.

---
+++ tests/Unit/Expressions/ForClasses/DummyClasses/Banana.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arkitect\Tests\Unit\Expressions\ForClasses\DummyClasses;
+
+class Banana implements FruitInterface
+{
+
+}
---
+++ tests/Unit/Expressions/ForClasses/DummyClasses/CavendishBanana.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arkitect\Tests\Unit\Expressions\ForClasses\DummyClasses;
+
+class CavendishBanana extends Banana
+{
+
+}
