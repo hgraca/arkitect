7bafac4be9f4128f3379a9b93649d9c7ef4f50a1
Herberto Graca <herberto.graca@gmail.com>
2023-09-16 17:40:37 +0100

    Create Expression DependsOnlyOnTheseExpressions

--- src/Analyzer/ClassDependency.php
+++ src/Analyzer/ClassDependency.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Arkitect\Analyzer;
 
+use Arkitect\Exceptions\ClassFileNotFoundException;
+
 class ClassDependency
 {
     private int $line;
@@ -39,5 +41,52 @@ class ClassDependency
     public function getFQCN(): FullyQualifiedClassName
     {
         return $this->FQCN;
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    public function getClassDescription(): ClassDescription
+    {
+        /** @var class-string $dependencyFqcn */
+        $dependencyFqcn = $this->getFQCN()->toString();
+        $reflector = new \ReflectionClass($dependencyFqcn);
+        $filename = $reflector->getFileName();
+        if (false === $filename) {
+            if (class_exists($dependencyFqcn)) {
+                return $this->createDescriptionOfNativeClass($reflector);
+            }
+            throw new ClassFileNotFoundException($dependencyFqcn);
+        }
+
+        $fileParser = FileParserFactory::createFileParser();
+        $fileParser->parse(file_get_contents($filename), $filename);
+        $classDescriptionList = $fileParser->getClassDescriptions();
+
+        return array_pop($classDescriptionList);
+    }
+
+    private function createDescriptionOfNativeClass(\ReflectionClass $reflector): ClassDescription
+    {
+        /** @var class-string $enumRootInterface */
+        $enumRootInterface = 'UnitEnum';
+        $isEnum = interface_exists($enumRootInterface) && $reflector->implementsInterface($enumRootInterface);
+
+        $classDescription = ClassDescription::getBuilder($reflector->getName(), '')
+            ->setFinal($reflector->isFinal())
+            ->setAbstract($reflector->isAbstract())
+            ->setInterface($reflector->isInterface())
+            ->setTrait($reflector->isTrait())
+            ->setEnum($isEnum);
+
+        $implementedInterfaceLit = array_map(
+            fn (\ReflectionClass $interface) => FullyQualifiedClassName::fromString($interface->getName()),
+            $reflector->getInterfaces()
+        );
+        foreach ($implementedInterfaceLit as $i => $interface) {
+            $classDescription->addInterface($interface->toString(), (int) $i + 10);
+        }
+
+        return $classDescription->build();
     }
 }
