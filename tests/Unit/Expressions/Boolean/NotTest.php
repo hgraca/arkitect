34a61a53919cda1073d34a0cc6291cb8251590b2
Herberto Graca <herberto.graca@gmail.com>
2025-04-17 22:11:23 +0200

    Create a Not expression
    This will reduce the amount of expressions needed
    and allow for complex expressions (within an OR
    or an AND) to be negated.

---
+++ tests/Unit/Expressions/Boolean/NotTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arkitect\Tests\Unit\Expressions\Boolean;
+
+use Arkitect\Analyzer\ClassDescriptionBuilder;
+use Arkitect\Expression\Boolean\Not;
+use Arkitect\Expression\ForClasses\IsInterface;
+use Arkitect\Rules\Violations;
+use PHPUnit\Framework\TestCase;
+
+class NotTest extends TestCase
+{
+    public function test_it_should_return_violation_error(): void
+    {
+        $isNotInterface = new Not(new IsInterface());
+        $classDescription = (new ClassDescriptionBuilder())
+            ->setFilePath('src/Foo.php')
+            ->setClassName('HappyIsland')
+            ->setInterface(true)
+            ->build();
+        $because = 'we want to add this rule for our software';
+        $violationError = $isNotInterface->describe($classDescription, $because)->toString();
+
+        $violations = new Violations();
+        $isNotInterface->evaluate($classDescription, $violations, $because);
+        self::assertNotEquals(0, $violations->count());
+
+        self::assertEquals(
+            'NOT (HappyIsland should be an interface) because we want to add this rule for our software',
+            $violationError
+        );
+    }
+
+    public function test_it_should_return_true_if_is_not_interface(): void
+    {
+        $isNotInterface = new Not(new IsInterface());
+        $classDescription = (new ClassDescriptionBuilder())
+            ->setFilePath('src/Foo.php')
+            ->setClassName('HappyIsland')
+            ->build();
+        $because = 'we want to add this rule for our software';
+        $violations = new Violations();
+        $isNotInterface->evaluate($classDescription, $violations, $because);
+        self::assertEquals(0, $violations->count());
+    }
+}
