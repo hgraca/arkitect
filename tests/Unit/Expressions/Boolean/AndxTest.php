6815f127730489f46702354e4d1be76f9baf7ca7
Herberto Graca <herberto.graca@gmail.com>
2025-04-17 22:10:53 +0200

    Create an And expression
    This will allow for complex nested expressions, like ANDs inside ORs.

---
+++ tests/Unit/Expressions/Boolean/AndxTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arkitect\Tests\Unit\Expressions\Boolean;
+
+use Arkitect\Analyzer\ClassDescriptionBuilder;
+use Arkitect\Expression\Boolean\Andx;
+use Arkitect\Expression\ForClasses\Extend;
+use Arkitect\Expression\ForClasses\Implement;
+use Arkitect\Rules\Violations;
+use PHPUnit\Framework\TestCase;
+
+class AndxTest extends TestCase
+{
+    public function test_it_should_pass_the_rule(): void
+    {
+        $interface = 'interface';
+        $class = 'SomeClass';
+        $classDescription = (new ClassDescriptionBuilder())
+            ->setFilePath('src/Foo.php')
+            ->setClassName('HappyIsland')
+            ->addInterface($interface, 11)
+            ->addExtends($class, 10)
+            ->build();
+        $implementConstraint = new Implement($interface);
+        $extendsConstraint = new Extend($class);
+        $andConstraint = new Andx($implementConstraint, $extendsConstraint);
+
+        $because = 'reasons';
+        $violations = new Violations();
+        $andConstraint->evaluate($classDescription, $violations, $because);
+
+        self::assertEquals(0, $violations->count());
+    }
+
+    public function test_it_should_pass_the_rule_when_and_is_empty(): void
+    {
+        $interface = 'interface';
+        $class = 'SomeClass';
+        $classDescription = (new ClassDescriptionBuilder())
+            ->setFilePath('src/Foo.php')
+            ->setClassName('HappyIsland')
+            ->addInterface($interface, 11)
+            ->addExtends($class, 10)
+            ->build();
+        $andConstraint = new Andx();
+
+        $because = 'reasons';
+        $violations = new Violations();
+        $andConstraint->evaluate($classDescription, $violations, $because);
+
+        self::assertEquals(0, $violations->count());
+    }
+
+    public function test_it_should_not_pass_the_rule(): void
+    {
+        $interface = 'SomeInterface';
+        $class = 'SomeClass';
+
+        $classDescription = (new ClassDescriptionBuilder())
+            ->setFilePath('src/Foo.php')
+            ->setClassName('HappyIsland')
+            ->addInterface($interface, 11)
+            ->build();
+
+        $implementConstraint = new Implement($interface);
+        $extendsConstraint = new Extend($class);
+        $andConstraint = new Andx($implementConstraint, $extendsConstraint);
+
+        $because = 'reasons';
+        $violationError = $andConstraint->describe($classDescription, $because)->toString();
+
+        $violations = new Violations();
+        $andConstraint->evaluate($classDescription, $violations, $because);
+        self::assertNotEquals(0, $violations->count());
+
+        self::assertEquals(
+            "(\nshould implement SomeInterface because reasons\nAND\nshould extend one of these classes: SomeClass because reasons\n) because reasons",
+            $violationError
+        );
+        self::assertEquals(
+            "The class 'HappyIsland' violated the expression\n"
+            ."should extend one of these classes: SomeClass, but (\n"
+            ."should implement SomeInterface because reasons\n"
+            ."AND\n"
+            ."should extend one of these classes: SomeClass because reasons\n"
+            .') because reasons',
+            $violations->get(0)->getError()
+        );
+    }
+}
